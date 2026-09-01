e5c4c6aed70bb802fe5385c8d9b758d8a0e04b84
Seth Acosta <acostavseth@gmail.com>
2026-09-01 13:54:51 -1000

    Fix PDF extraction stalls

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TranscriptScreener from './TranscriptScreener_Beautiful';
+import './pdfSetup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
